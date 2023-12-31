04f4086080a77371704c9716b3631b6d1998b48d
jadashi97 <njadauniversal23@yahoo.com>
2023-12-31 11:04:29 -0600

    (feat): added to the webcrawler project

--- crawl.js
+++ crawl.js
@@ -1,6 +1,15 @@
-const normalizedURL = ()=>{
-    console.log("Yay gotcha!!");
-};
+// const normalizedURL = ()=>{
+//     console.log("Yay gotcha!!");
+// };
+
+function normalizedURL(url){
+    const urlObj = new URL(url)
+    let fullPath = `${urlObj.host}${urlObj.pathname}}`
+    if(fullPath.length > 0 && fullPath.slice(-1) === '/'){
+        fullPath = fullPath.slice(0, -1)
+    }
+    return fullPath
+}
 
 module.exports = {
     normalizedURL
