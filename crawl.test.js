19ed6e276e2378e767037b0bdcbb4b19e7d57a50
jadashi97 <njadauniversal23@yahoo.com>
2024-01-02 22:22:35 -0600

    (feat): learn the  in and outs of how the web crawler works

--- crawl.test.js
+++ crawl.test.js
@@ -1,2 +1,9 @@
-import { test, expect } from '@jest/globals';
-import { normalizeURL } from './crawl.js';
+const { test, expect } =  require('@jest/globals');
+const { normalizeURL, getURLsFromHTML} =  require('./crawl.js');
+
+test('normalizeURL protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
